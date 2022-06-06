81bc464d3c9aa245149ea56dc50369c4acdfb06c
Ahamedmk <amkavavo1@gmail.com>
2022-06-06 23:52:29 +0200

    finalisé

---
+++ projet2-boutton abonné/script.js
@@ -0,0 +1,20 @@
+console.log('connecté !')
+const icone = document.querySelector('i');
+console.log(icone);
+const btnButton = document.querySelector('.btn');
+console.log(btnButton);
+icone.addEventListener('click', function(){
+    console.log('icone cliqué');
+    icone.classList.toggle('fa-smile-wink');
+    icone.classList.toggle('happy');
+})
+btnButton.addEventListener('click', function(){
+    console.log('yes');
+    btnButton.classList.toggle('abonne');
+    if(btnButton.innerText === "abonné"){
+        btnButton.innerText = "abonnez vous";
+    }else{
+        btnButton.innerText = "abonné";
+    };
+    
+});
